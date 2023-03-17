67a9c800b13263d9a8381dbb1a5693623b2a9669
marked-gil <markgil_creates@yahoo.com>
2023-03-17 12:55:32 +0000

    Create Task model and set up admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """ """
+    list_display = ('id', 'owner', 'task_name', 'details',
+                    'due_date', 'due_time', 'progress')
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Model for the Tasks' table in the database
+    """
+    PROGRESS = [
+        ("overdue", "Overdue"),
+        ("todo", "Todo")
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_name = models.CharField(
+        max_length=250, blank=False, editable=True, unique_for_date="due_date"
+    )
+    details = models.TextField(max_length=1000, blank=True, editable=True)
+    # category = models.CharField(choices=)
+    due_date = models.DateField(blank=False)
+    due_time = models.TimeField(blank=True)
+    progress = models.CharField(max_length=15, choices=PROGRESS,
+                                default="todo")
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Arranges the Task's instances based on their due dates
+        """
+        ordering = ['-due_date']
+
+    def __str__(self):
+        """
+        Returns the task name of the Task's instance
+        """
+        return self.task_name
