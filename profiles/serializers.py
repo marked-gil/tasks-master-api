0e7d477a2f159892b2088d2c4afe019e0fd4e56d
marked-gil <markgil_creates@yahoo.com>
2023-03-17 13:31:10 +0000

    Create Profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'first_name', 'last_name', 'email', 'image',
+            'datetime_created', 'datetime_updated'
+        ]
