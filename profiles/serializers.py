96044d7dae2c2acd99426b8dc74a79cff9919f9e
marked-gil <markgil_creates@yahoo.com>
2023-03-17 16:04:55 +0000

    Create ProfileDetails view and GET & UPDATE methods to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,8 +3,12 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
+        """
+        Specifies the fields returned by the API
+        """
         model = Profile
         fields = [
             'id', 'owner', 'first_name', 'last_name', 'email', 'image',
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class ProfileDetails(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+    lookup_url_kwarg = 'id'
