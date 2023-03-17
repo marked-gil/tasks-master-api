83c003254f0025c99928bfc53faeb67e2fa0ad6a
marked-gil <markgil_creates@yahoo.com>
2023-03-17 23:43:41 +0000

    Update format of owner and datetime fields in CategorySerializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -4,6 +4,15 @@ from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
     """ Serializer for Category Model """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    datetime_created = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
+    datetime_updated = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
 
     class Meta:
         """ Specifies the fields returned by the API """
