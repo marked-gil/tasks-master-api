01887c9f37f136d28daaa96c1b1471038c6a5bc9
marked-gil <markgil_creates@yahoo.com>
2023-03-18 02:04:58 +0000

    Add the root route message

--- tasks_master_api/urls.py
+++ tasks_master_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
     path('', include('tasks.urls')),
