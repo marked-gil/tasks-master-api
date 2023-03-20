068b1c7e9d6bcc4b49b4aea90c1b94e129109e0e
marked-gil <markgil_creates@yahoo.com>
2023-03-20 18:23:55 +0000

    Change format for datetime in comments serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,6 +4,16 @@ from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     """ Serializer for Comment model """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    datetime_created = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
+    datetime_updated = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
+
     class Meta:
         """ Specifies the fields returned by the API """
         model = Comment
