d499ec394f95ee671f89b020b79d01563632606d
marked-gil <markgil_creates@yahoo.com>
2023-03-20 09:12:29 +0000

    Create SharedTask serializer

---
+++ shared_tasks/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import SharedTask
+
+
+class SharedTaskSerializer(serializers.ModelSerializer):
+    """ """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    task = serializers.ReadOnlyField(source='task.task_name')
+    datetime_created = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
+    datetime_updated = serializers.DateTimeField(
+        format="%b %d, %Y | %H:%M",
+        read_only=True
+    )
+
+    class Meta:
+        """ """
+        model = SharedTask
+        fields = [
+            'id', 'owner', 'task', 'shared_to', 'datetime_created',
+            'datetime_updated'
+        ]
