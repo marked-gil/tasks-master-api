f09caa0d5ea7d2c928becb4255298097be9f5da0
marked-gil <markgil_creates@yahoo.com>
2023-03-20 15:37:48 +0000

    Allow sharedtask owner to delete object, while read-only to other authenticated users

--- shared_tasks/views.py
+++ shared_tasks/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import SharedTask
 from .serializers import SharedTaskSerializer
 from django.db.models import Q
+from tasks_master_api.permissions import IsOwner, IsAuthenticatedReadOnly
 
 
 class SharedTaskList(generics.ListCreateAPIView):
@@ -30,11 +31,12 @@ class SharedTaskDetails(generics.RetrieveDestroyAPIView):
     Retrieves and deletes a single SharedTask instance
     """
     serializer_class = SharedTaskSerializer
+    permission_classes = [IsOwner | IsAuthenticatedReadOnly]
     lookup_url_kwarg = 'id'
 
     def get_queryset(self):
         """ Returns a single task created by the current user """
         user = self.request.user
         return SharedTask.objects.filter(
-            Q(shared_to=user.id) | Q(owner=user)
+            Q(shared_to=user.id) | Q(owner=user.id)
         ).distinct()
--- tasks_master_api/permissions.py
+++ tasks_master_api/permissions.py
@@ -1,6 +1,19 @@
 from rest_framework import permissions
 
 
+class IsOwner(permissions.BasePermission):
+    """ Custom permission for instance owner """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsAuthenticatedReadOnly(permissions.BasePermission):
+    """ Read-only permission for authenticated users """
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.method == 'GET':
+            return True
+
+
 class IsOwnerIsAuthenticated(permissions.BasePermission):
     """
     Custom permission to allow update and delete to the owner only,
