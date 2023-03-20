331230dcfe0165f1c5cdab89b00fa402d5e2d9b1
marked-gil <markgil_creates@yahoo.com>
2023-03-20 13:01:47 +0000

    Return shared_to users with their usernames

--- shared_tasks/serializers.py
+++ shared_tasks/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import SharedTask
 
 
 class SharedTaskSerializer(serializers.ModelSerializer):
     """ """
     owner = serializers.ReadOnlyField(source='owner.username')
+    shared_to = serializers.SlugRelatedField(
+        many=True,
+        slug_field='username',
+        queryset=User.objects.all()
+    )
     datetime_created = serializers.DateTimeField(
         format="%b %d, %Y | %H:%M",
         read_only=True
