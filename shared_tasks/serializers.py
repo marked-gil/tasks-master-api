f4269faece088cd26c4feeae3f71973038dbf59f
marked-gil <markgil_creates@yahoo.com>
2023-03-20 13:44:52 +0000

    Display the task's name, and create a task_id field

--- shared_tasks/serializers.py
+++ shared_tasks/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import SharedTask
+from tasks.models import Task
 
 
 class SharedTaskSerializer(serializers.ModelSerializer):
     """ """
     owner = serializers.ReadOnlyField(source='owner.username')
+    task = serializers.SlugRelatedField(
+        slug_field='task_name',
+        queryset=Task.objects.all()
+    )
+    task_id = serializers.SerializerMethodField()
     shared_to = serializers.SlugRelatedField(
         many=True,
         slug_field='username',
@@ -20,10 +26,16 @@ class SharedTaskSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
+    def get_task_id(self, obj):
+        """
+        Gets the id of the task
+        """
+        return obj.task.id
+
     class Meta:
-        """ """
+        """ Specifies the fields returned by the API """
         model = SharedTask
         fields = [
-            'id', 'owner', 'task', 'shared_to', 'datetime_created',
+            'id', 'owner', 'task', 'task_id', 'shared_to', 'datetime_created',
             'datetime_updated'
         ]
--- shared_tasks/views.py
+++ shared_tasks/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import SharedTask
 from .serializers import SharedTaskSerializer
+from django.db.models import Q
 
 
 class SharedTaskList(generics.ListCreateAPIView):
@@ -15,7 +16,7 @@ class SharedTaskList(generics.ListCreateAPIView):
         Returns all shared tasks created by the current user
         """
         user = self.request.user
-        return SharedTask.objects.filter(owner=user)
+        return SharedTask.objects.filter(Q(shared_to=user.id) | Q(owner=user))
 
     def perform_create(self, serializer):
         """ Sets the current user as the owner """
