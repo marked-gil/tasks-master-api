c76e7d6e0b5236924fb8e1c664836c2815524385
marked-gil <markgil_creates@yahoo.com>
2023-03-20 10:23:44 +0000

    Automatically set the current user as owner of the instance in the admin

--- shared_tasks/admin.py
+++ shared_tasks/admin.py
@@ -5,7 +5,12 @@ from .models import SharedTask
 @admin.register(SharedTask)
 class SharedTaskAdmin(admin.ModelAdmin):
     """ Sets up the admin display for SharedTask model """
-    list_display = ('id', 'task', 'datetime_created',
+    list_display = ('id', 'owner', 'task', 'datetime_created',
                     'datetime_updated')
     exclude = ('owner',)
     filter_horizontal = ['shared_to',]
+
+    def save_model(self, request, obj, form, change):
+        """ Automatically sets the current user as owner of the instance """
+        obj.owner = request.user
+        super().save_model(request, obj, form, change)
