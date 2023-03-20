ec3b90b525f1eb910c2822470592c23ba46fb1f6
marked-gil <markgil_creates@yahoo.com>
2023-03-20 23:48:56 +0000

    Create CommentDetails view and improve filter in CommentList

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from django.db.models import Q
+from shared_tasks.models import SharedTask
 from .models import Comment
 from shared_tasks.models import SharedTask
 from .serializers import CommentSerializer
@@ -11,8 +12,33 @@ class CommentList(generics.ListCreateAPIView):
     Returns a list of comments, and creates new comments
     """
     serializer_class = CommentSerializer
-    queryset = Comment.objects.all()
+
+    def get_queryset(self):
+        """ Returns a single comment """
+        user = self.request.user
+        tasks = SharedTask.objects.filter(shared_to=user.id).values('task_id')
+        return Comment.objects.filter(
+            Q(reply_to=user.id) | Q(owner=user.id) |
+            Q(task__id__in=tasks)
+        ).distinct()
 
     def perform_create(self, serializer):
         """ Sets the current user as the owner """
         serializer.save(owner=self.request.user)
+
+
+class CommentDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieves, updates and deletes a single Comment instance
+    """
+    serializer_class = CommentSerializer
+    lookup_url_kwarg = 'id'
+
+    def get_queryset(self):
+        """ Returns a single comment """
+        user = self.request.user
+        tasks = SharedTask.objects.filter(shared_to=user.id).values('task_id')
+        return Comment.objects.filter(
+            Q(reply_to=user.id) | Q(owner=user.id) |
+            Q(task__id__in=tasks)
+        ).distinct()
