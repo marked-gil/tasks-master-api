648b92c42a2a0c6c1fabc2002c2e7b3d95a0d109
marked-gil <markgil_creates@yahoo.com>
2023-03-21 01:09:03 +0000

    Create custom serializer for category field in Task

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
+from categories.models import Category
 from .models import Task
+
+
+class CategorySlugRelatedSerializer(serializers.SlugRelatedField):
+    """
+    Custom serializer for category field
+    [Idea taken from StackOverflow (See 'Credits' in README.md)]
+    """
+    def get_queryset(self):
+        """ Retrieves Category querysets created by current user """
+        queryset = Category.objects.all()
+        request = self.context.get('request', None)
+        return queryset.filter(owner=request.user)
 
 
 class TaskSerializer(serializers.ModelSerializer):
     """ Serializer for Task Model """
     owner = serializers.ReadOnlyField(source='owner.username')
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    category = CategorySlugRelatedSerializer(slug_field='category_name')
     due_date = serializers.DateField(format="%d %B %Y")
     due_time = serializers.TimeField(format="%I:%M %p", required=False)
     progress = serializers.ReadOnlyField()
