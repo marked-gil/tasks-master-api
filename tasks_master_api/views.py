a5acb58d6df5ccba05b9923f6d169feae75519bc
marked-gil <markgil_creates@yahoo.com>
2023-03-22 16:00:52 +0000

    Add fix to logout bug in dj-rest-auth

--- tasks_master_api/views.py
+++ tasks_master_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import REST_AUTH
 
 
 @api_view()
@@ -7,3 +8,31 @@ def root_route(request):
     return Response({
         'message': 'You have reached the API for Tasks Master'
     })
+
+
+@api_view(['POST'])
+def logout_route(request):
+    """
+    Fix to dj-rest-auth logout view
+    [As suggested from CI drf_api walkthrough project]
+    """
+    response = Response()
+    response.set_cookie(
+        key=REST_AUTH.JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=REST_AUTH.JWT_AUTH_SAMESITE,
+        secure=REST_AUTH.JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=REST_AUTH.JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=REST_AUTH.JWT_AUTH_SAMESITE,
+        secure=REST_AUTH.JWT_AUTH_SECURE,
+    )
+    return response
