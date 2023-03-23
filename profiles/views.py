97b2292a3b7fadc45a24a2d2effa7cc007dba15b
marked-gil <markgil_creates@yahoo.com>
2023-03-23 01:17:59 +0000

    Allow deletion of profile/account

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .models import Profile
 from .serializers import ProfileSerializer
 from tasks_master_api.permissions import IsOwner, IsAuthenticatedReadOnly
@@ -14,12 +15,17 @@ class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.all()
 
 
-class ProfileDetails(generics.RetrieveUpdateAPIView):
+class ProfileDetails(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieves (for authenticated users) and Updates (for owners only)
+    Retrieves (for authenticated users), Updates and deletes (for owners only)
     current user's profile
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
     permission_classes = [IsOwner | IsAuthenticatedReadOnly]
     lookup_url_kwarg = 'id'
+    queryset = Profile.objects.all()
+
+    def perform_destroy(self, instance):
+        """ Deletes both the user account and their profile """
+        instance.delete()
+        User.objects.get(id=instance.owner.id).delete()
