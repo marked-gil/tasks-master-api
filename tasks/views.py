f0825a7c1e8a8f3a8811b9ced74238bd26d5745c
marked-gil <markgil_creates@yahoo.com>
2023-04-05 03:13:19 +0000

    Filter tasks by shared_to

--- tasks/views.py
+++ tasks/views.py
@@ -19,7 +19,7 @@ class TaskList(generics.ListCreateAPIView):
         filters.OrderingFilter,
         DjangoFilterBackend
     ]
-    filterset_fields = ['due_date', 'priority', 'progress', 'category']
+    filterset_fields = ['due_date', 'priority', 'progress', 'category', 'shared_to']
     ordering_fields = ['due_date', 'due_time', 'priority']
     search_fields = ['category__category_name', 'task_name']
 
