d41f3db0f4d7ae22ae3d5fd040afc5333461c548
marked-gil <markgil_creates@yahoo.com>
2023-04-16 22:34:55 +0000

    Add JWT_AUTH_HTTPONLY = True

--- tasks_master_api/settings.py
+++ tasks_master_api/settings.py
@@ -46,6 +46,7 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+JWT_AUTH_HTTPONLY = True
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'tasks_master_api.serializers.'
