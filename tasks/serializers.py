85c21ce3e551aa5af3a2c75febdd494c08d6fcd7
marked-gil <markgil_creates@yahoo.com>
2023-04-20 18:32:55 +0000

    Use ProfileSerializer for shared_to field

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -5,6 +5,7 @@ from datetime import date, datetime
 from categories.models import Category
 from .models import Task
 from profiles.models import Profile
+from profiles.serializers import ProfileSerializer
 
 
 class CategorySlugSerializer(serializers.SlugRelatedField):
@@ -27,11 +28,7 @@ class TaskSerializer(serializers.ModelSerializer):
     category = CategorySlugSerializer(slug_field='category_name')
     progress = serializers.ReadOnlyField()
     due_time = serializers.TimeField(format="%H:%M", allow_null=True)
-    shared_to = serializers.SlugRelatedField(
-        many=True,
-        slug_field='username',
-        queryset=User.objects.all()
-    )
+    shared_to = ProfileSerializer(many=True, read_only=True)
     datetime_created = serializers.DateTimeField(read_only=True)
     datetime_updated = serializers.DateTimeField(read_only=True)
 
