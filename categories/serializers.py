37fe407420efd42e506ca0b4ce29e6a778cfab32
marked-gil <markgil_creates@yahoo.com>
2023-04-25 18:42:27 +0000

    Serialize the unique_together fields for Category model

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 from .models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """ Serializer for Category Model """
-    owner = serializers.ReadOnlyField(source='owner.username')
+    username = serializers.SerializerMethodField()
     datetime_created = serializers.DateTimeField(read_only=True)
     datetime_updated = serializers.DateTimeField(read_only=True)
 
+    def get_username(self, obj):
+        """ Gets the username of the owner """
+        return obj.owner.username
+
     class Meta:
-        """ Specifies the fields returned by the API """
+        """
+        Specifies the fields returned by the API and validates posted and
+        updated category
+        """
         model = Category
         fields = [
-            'id', 'owner', 'category_name', 'description', 'datetime_created',
-            'datetime_updated'
+            'id', 'owner', 'username', 'category_name', 'description',
+            'datetime_created', 'datetime_updated'
+        ]
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=model.objects.all(),
+                fields=('owner', 'category_name'),
+                message=_("The same category already exists. Try a new one.")
+            )
         ]
